aeea07fe532402a650bbc147e3751f43b674035c
mariusz <mariusz.waldoch98@gmail.com>
2022-07-27 22:02:02 +0200

    sign in form

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,7 +1,32 @@
 <template>
   <div class="container-fluid px-0">
     <div class="cont">
-      <h2>Login form here</h2>
+      <form @submit.prevent>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email </label>
+          <input
+            type="email"
+            class="form-control bg-dark text-white"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" class="form-text text-light">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Hasło</label>
+          <input
+            type="password"
+            class="form-control bg-dark text-white"
+            id="exampleInputPassword1"
+          />
+        </div>
+
+        <button type="submit" class="btn btn-danger">
+          <strong>Zaloguj</strong>
+        </button>
+      </form>
     </div>
   </div>
 </template>
@@ -25,11 +50,26 @@ export default defineComponent({
   position: relative;
 
   .cont {
-    margin-top: 45vh;
+    margin-top: 35vh;
     color: white;
-    text-align: center;
     position: absolute;
-    width: 100%;
+    width: 40%;
+    margin-inline: 30%;
+    background-color: rgba(0, 0, 0, 0.7);
+    border-radius: 10px;
+    form {
+      padding-inline: 20%;
+      margin-block: 10%;
+
+      input {
+        border-color: rgb(20, 20, 20);
+      }
+
+      .btn {
+        background-color: red;
+        width: 100%;
+      }
+    }
   }
 }
 </style>
